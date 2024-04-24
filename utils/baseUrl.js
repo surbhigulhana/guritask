d9bce11905eab1e426dcdc692e4cd4432de24a8f
surbhi <surbhigulhana3@gmail.com>
2024-04-24 14:53:00 +0530

    update

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
 	process.env.NODE_ENV === "production"
-		? "https://elearniv-react-next-i5p7.vercel.app"
-		: "https://elearniv-react-next-i5p7.vercel.app/";
+		? "https://elearniv-react.envytheme.com"
+		: "http://localhost:3000";
 
 export default baseUrl;
